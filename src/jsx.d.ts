800346f75cf13d89a47326629c2fde1aa8dce6e1
Jonathan Eric Niemann <jniemann87@gmail.com>
2025-04-29 03:25:28 -0300

    jsx types

---
+++ src/jsx.d.ts
@@ -0,0 +1,16 @@
+import "react";
+
+declare module "react" {
+  interface HTMLAttributes {
+    pload?: string;
+    "t-white"?: string;
+    "easter-fade"?: string;
+    "h-effect"?: string;
+    "t-black"?: string;
+    "mask-in"?: string;
+    "hover-fade"?: string;
+    "hidemob-logo"?: string;
+    "msingle"?: string;
+    "archive-sync"?: string;
+  }
+}
